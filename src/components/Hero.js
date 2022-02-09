b543fc85e2b5b2a4bd237260ff4c12e906f20581
Konrad Urbańczyk <elearning.warsaw@gmail.com>
2022-02-09 09:31:53 +0100

    changed font and pic

--- src/components/Hero.js
+++ src/components/Hero.js
@@ -1,63 +1,79 @@
-import React from 'react'
+import React from "react"
 import { Container, Row, OverlayTrigger, Tooltip } from "react-bootstrap"
 import socialLinks from "../constants/social_links"
 import { Link } from "gatsby"
 import { StaticImage } from "gatsby-plugin-image"
-
+import HeroPic from "../assets/svg/undraw_programming_re_kg9v.svg"
+import styled from "styled-components"
 
 const Hero = () => {
-    return (
-             <section id="home" className="hero">
-      <Container className="my-container">
-        <Row className="full-screen align-items-center p-100px-tb hero-center">
-          <div className="hero-info">
-            <h6>Dzień dobry! Nazywam się...</h6>
-            <h1 className="my-2">Konrad Urbańczyk</h1>
-            <h3>Web developer</h3>
-            <div className="btn-bar mb-2">
-              <Link className="m-btn m-btn-theme" to="#portfolio">
-               Portfolio
-              </Link>
-              <Link className="m-btn m-btn-t-theme" to="#kontakt">
-                Kontakt
-              </Link>
-            </div>
-            <ul className="social-links hero-icons">
-              {socialLinks.map(link => {
-                return (
-                  <OverlayTrigger
+  return (
+    <Wrapper id="home" className="hero">
+      <Container className="full-screen d-flex align-items-center p-100px-tb hero-center">
+        <div className="hero-info col-7 ">
+          <h6>Dzień dobry! Nazywam się...</h6>
+          <h2 className="my-2">Konrad Urbańczyk</h2>
+          <h1>Web developer</h1>
+          <p className="mt-4 mb-2 col-6">Some text here about me how i am a great guy awesome.Some text here about me how i am a great guy awesome.</p>
+          <div className="btn-bar mb-2">
+            <Link className="m-btn m-btn-theme" to="#portfolio">
+              Portfolio
+            </Link>
+            <Link className="m-btn m-btn-t-theme" to="#kontakt">
+              Kontakt
+            </Link>
+          </div>
+          <ul className="social-links hero-icons">
+            {socialLinks.map(link => {
+              return (
+                <OverlayTrigger
                   key={link.id}
-                    placement="bottom"
-                    delay={{ show: 200, hide: 200 }}
-                    overlay={<Tooltip id="button-tooltip">{link.name}</Tooltip>}
-                  >
-                    <li >
-                      <a href={link.url} className="social-link">
-                        {link.icon}
-                      </a>
-                    </li>
-                  </OverlayTrigger>
-                )
-              })}
-            </ul>
-          </div>
-          <div className="hero-img">
-            <StaticImage
-              src="../images/my-shape2.jpg"
-              alt="Konrad Urbanczyk"
-              className="hero-img"
-              layout="constrained"
-              quality="100"
-              style={{ marginBottom: `1.45rem` }}
-            />
-          </div>
-        </Row>
+                  placement="bottom"
+                  delay={{ show: 200, hide: 200 }}
+                  overlay={<Tooltip id="button-tooltip">{link.name}</Tooltip>}
+                >
+                  <li>
+                    <a href={link.url} className="social-link">
+                      {link.icon}
+                    </a>
+                  </li>
+                </OverlayTrigger>
+              )
+            })}
+          </ul>
+        </div>
+          <HeroPic className='col-6' />
       </Container>
       <div className="icon-scroll-container">
         <Link className="icon-scroll" to="#portfolio"></Link>
       </div>
-    </section>
-    )
+    </Wrapper>
+  )
 }
+
+const Wrapper = styled.section`
+
+
+h6{
+  font-weight:400;
+  color:var(--clr-primary);
+  font-size:18px;
+}
+
+h2{
+  font-weight:300;
+  font-size:72px;
+  line-height:1;
+}
+
+h1{
+  font-weight:bold;
+  font-size:90px;
+  line-height:1;
+
+}
+
+
+`
 
 export default Hero
